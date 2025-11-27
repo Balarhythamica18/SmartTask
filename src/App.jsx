9b5b17f344791a45d3eec73bf7e611689343c63e
Balarhythamica18 <balarhythamica@gmail.com>
2025-11-27 15:46:35 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -16,10 +16,33 @@ export default function App() {
         <Layout>
           <Routes>
             <Route path="/login" element={<Login />} />
-            <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
-            <Route path="/tasks" element={<ProtectedRoute><Tasks /></ProtectedRoute>} />
-            <Route path="/profile" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
-            <Route path="/" element={<Navigate to="/dashboard" replace />} />
+            <Route
+              path="/dashboard"
+              element={
+                <ProtectedRoute>
+                  <Dashboard />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/tasks"
+              element={
+                <ProtectedRoute>
+                  <Tasks />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/profile"
+              element={
+                <ProtectedRoute>
+                  <Profile />
+                </ProtectedRoute>
+              }
+            />
+
+            {/* 👇 When page loads, redirect to Login */}
+            <Route path="/" element={<Navigate to="/login" replace />} />
           </Routes>
         </Layout>
       </AuthProvider>
